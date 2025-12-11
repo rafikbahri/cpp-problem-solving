2dc7b28570713ca697798ef249de5554aed0bd3a
Rafik Bahri <rafikbahri.insat@gmail.com>
2025-12-12 00:03:49 +0100

    bucket sort

--- neetcode/course/beginner-intermidiate/sorts/bucket.cpp
+++ neetcode/course/beginner-intermidiate/sorts/bucket.cpp
@@ -4,29 +4,24 @@ using namespace std;
 
 class Solution{
     public:
-        void bucketSort(vector<int>& arr){
-            // calculate range
-            int max = *max_element(arr.begin(), arr.end());
-            int min = *min_element(arr.begin(), arr.end());
+        void bucketSort(vector<int>& nums) {
+            int max = *max_element(nums.begin(), nums.end());
+            int min = *min_element(nums.begin(), nums.end());
             int rangeSize = max - min + 1;
-
-            cout<<"max: "<<max<<endl;
-            cout<<"min: "<<min<<endl;
-            cout<<"range size: "<<rangeSize<<endl;
             vector<int> buckets(rangeSize, 0);
-
-            // create buckets
-            for(int i = 0; i < arr.size(); i++)
-                buckets[arr[i]]++;
-
+        
+            // Map value to bucket index: value - min
+            for(int i = 0; i < nums.size(); i++){
+                buckets[nums[i] - min]++;
+            }
+        
             int idx = 0;
-            for(int n = 0; n<buckets.size(); n++){
-                for(int i=0; i < buckets[n]; i++){
-                    arr[idx] = n;
+            for(int n = 0; n < buckets.size(); n++){
+                for(int i = 0; i < buckets[n]; i++){
+                    nums[idx] = n + min;  // Map bucket index back to value
                     idx++;
                 }
-                
-            }
+            }    
         }
 
         void show(vector<int> arr){
@@ -44,7 +39,8 @@ int main(){
     // obj->bucketSort(in1);
     // cout<<"Output: "<<endl;
     // obj->show(in1);
-    vector<int> in1 = {3, 0, 2, 0, 0, 1, 7, 1000000, 1};
+    // vector<int> in1 = {3, 0, 2, 0, 0, 1, 7, 1000000, 1};
+    vector<int> in1 = {1, 1};
     cout<<"Input: "<<endl;
     obj->show(in1);
     obj->bucketSort(in1);
